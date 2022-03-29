e7f69b57a3cfeece560407332a4c06f2e2688baf
vladgm5522 <61971715+vladgm5522@users.noreply.github.com>
2022-03-29 16:22:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ let settings = require('./settings.json'),
 	base = require("./base.json"),
 	chats = require('./chats.json');
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 const right = [
 	{
 		name: 'Коты',
